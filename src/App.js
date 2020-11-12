441be7e9e1a2ec6ed9b8222523924ddb9e5df7e2
Alexandra Tran <elexandra.tran@utoronto.ca>
2020-11-12 04:58:19 -0500

    Changed colour of Bar, changed title and buttons.

--- src/App.js
+++ src/App.js
@@ -8,18 +8,13 @@ function App() {
       <Bar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          <i class="far fa-angry"></i>
-        </a>
+        <p>Welcome to <code>sootysoot's</code> castle.</p>
+        <div className='logoContainer'>
+        <a href="https://www.linkedin.com/in/tran-alexandra/" className="logo"><i class="fab fa-linkedin fa-2x"></i></a>
+        <a href="https://github.com/alexandra-tran" className='logo'><i class="fab fa-github fa-2x"></i></a>
+        <a href="https://facebook.com/elexandra.tran" className='logo'><i class="fab fa-facebook fa-2x"></i></a>
+        <a href="https://instagram.com/woahalexisonfire" className='logo'><i class="fab fa-instagram fa-2x"></i></a>
+        </div>
       </header>
     </div>
   );
--- src/Components/Bar/Bar.js
+++ src/Components/Bar/Bar.js
@@ -1,34 +1,45 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { createMuiTheme,
+	createStyles,
+	withStyles,
+	makeStyles,
+	Theme,
+	ThemeProvider,} from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core/';
 import { AppBar, Toolbar, Typography, Button, IconButton, Menu }  from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu';
 
+
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+	root: {
+	  flexGrow: 1,
+	},
+	menuButton: {
+	  marginRight: theme.spacing(1),
+	},
+	title: {
+	  flexGrow: 1,
+	},
+	text: {
+		color: '#474a6e',
+	},
+	buttonText: {
+		color: '#474a6e',
+	}
+  }));
 
 export default function Bar() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="fixed" style={{ background: '#a8c5ff' }}>
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon/>
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Typography className={classes.text} variant="h6">e.t.</Typography>
+		  <Grid container justify="flex-end">
+     		<Button className={classes.buttonText}>Hello</Button>
+			<Button className={classes.buttonText}>Goodbye</Button>
+		  </Grid>
         </Toolbar>
       </AppBar>
     </div>

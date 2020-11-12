2b451f32b880bac4044eb5ef51d48d9f870d9a1e
Alexandra Tran <elexandra.tran@utoronto.ca>
2020-11-12 05:15:29 -0500

    Added GitLab button.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         <div className='logoContainer'>
         <a href="https://www.linkedin.com/in/tran-alexandra/" className="logo"><i class="fab fa-linkedin fa-2x"></i></a>
         <a href="https://github.com/alexandra-tran" className='logo'><i class="fab fa-github fa-2x"></i></a>
+        <a href="https://gitlab.com/alexandra-tran" className='logo'><i class="fab fa-gitlab fa-2x"></i></a>
         <a href="https://facebook.com/elexandra.tran" className='logo'><i class="fab fa-facebook fa-2x"></i></a>
         <a href="https://instagram.com/woahalexisonfire" className='logo'><i class="fab fa-instagram fa-2x"></i></a>
         </div>

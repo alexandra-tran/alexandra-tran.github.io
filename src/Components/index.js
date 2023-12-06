235ae1e097f9b6e49d83967df020b10f3666035f
Alexandra Tran <elexandra.tran@utoronto.ca>
2023-12-06 01:38:00 -0500

    add single file website

--- src/Components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Bar } from './Bar/Bar';
-export { default as NameCard } from './NameCard/NameCard';
-export { default as NestedList } from './NestedList/NestedList';
-export { default as Timeline } from './Timeline/Timeline';
-export { default as ProjectCard } from './ProjectCard/ProjectCard';
